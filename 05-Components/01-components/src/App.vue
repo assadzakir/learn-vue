18dba4f5ec08c5c7fb387e81cc8eba8b0b39f59c
Assad zakir <assadzakirhussain@gmail.com>
2017-06-18 17:12:15 +0500

    -vue Instance -vue lifecycle -vue components

---
+++ 05-Components/01-components/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <Recipes></Recipes>
+            <app-Recipe-details></app-Recipe-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Shared/Header.vue';
+    import Footer from './components/Shared/Footer.vue';
+    import Recipes from './components/Recipe/Recipes.vue';
+    import RecipeDetails from './components/Recipe/RecipeDetails.vue';
+
+    export default {
+        components: {
+            appHeader: Header,
+            Recipes,
+            'app-Recipe-details': RecipeDetails,
+            'app-footer': Footer
+        }
+    }
+</script>
+
+<style>
+
+</style>
