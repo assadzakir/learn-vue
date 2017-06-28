86ebe1b56e06009b00e85c9bb6d4b0f7afd8ee4a
Assad zakir <assadzakirhussain@gmail.com>
2017-06-28 16:52:53 +0500

    -Advance components usage

---
+++ 07-More-about-components/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent = 'appRecipe'">Recipe</button>
+                <button @click="selectedComponent = 'appChef'">Chef</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default Content</p>
+                    </component>
+                </keep-alive>
+                <app-recipe>
+                    <h2 slot="title">{{ recipeTitle }}</h2>
+                    <p>A wonderful recipe</p>
+                </app-recipe>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Recipe from './components/Recipe.vue';
+    import Chef from './components/Chef.vue';
+    import New from './components/New.vue';
+
+    export default {
+        data: function() {
+            return {
+                recipeTitle: 'The Recipe',
+                selectedComponent: 'appRecipe'
+            }
+        },
+        components: {
+            appRecipe: Recipe,
+            appChef: Chef,
+            appNew: New
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ 07-More-about-components/src/components/Chef.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h3>The Chef</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+</style>
