fab031a7b49ad204818755ee4b321f4503df6608
Assad zakir <assadzakirhussain@gmail.com>
2017-06-28 16:40:26 +0500

    -communication between components

---
+++ 06-Communication-between-Components/src/components/User.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="component">
+        <h1>Parent Component</h1>
+        <p>User!</p>
+        <button @click="changeName">Change my Name</button>
+        <p>Name is {{ name }}</p>
+        <p>Age is {{ age }}</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail
+                        :myName="name"
+                        @nameWasReset="name = $event"
+                        :resetFn="resetName"
+                        :userAge="age"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit
+                        :userAge="age"
+                        @ageWasEdited="age = $event"
+                ></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function () {
+            return {
+                name: 'Assad',
+                age: 25
+            };
+        },
+        methods: {
+            changeName() {
+                this.name = 'Jawad';
+            },
+            resetName() {
+                this.name = 'Mohsin';
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: gray;
+    }
+</style>
