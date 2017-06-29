bd25f672e9c7451e9b4194983f02f454b9e4abf3
Assad zakir <assadzakirhussain@gmail.com>
2017-06-29 12:36:07 +0500

    -Filters

---
+++ 10-Filters/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filters</h1>
+                <p>{{ text | toUppercase | to-lowercase }}</p>
+                <hr>
+                <input v-model="filterText">
+                <ul>
+                    <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad repellat porro enim expedita, veritatis ex distinctio ratione nostrum esse incidunt similique deserunt odio ullam ab accusantium, ducimus tempore recusandae facilis?',
+                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+                filterText: ''
+            }
+        },
+        filters: {
+            toUppercase(value) {
+                return value.toUpperCase();
+            }
+        },
+        computed: {
+            filteredFruits() {
+                return this.fruits.filter((element) => {
+                    return element.match(this.filterText);
+                });
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
